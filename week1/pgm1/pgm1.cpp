71d5e1346ff637659103dc4994da655ead37ada8
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2020-10-19 07:01:11 -0700

    Add files via upload

---
+++ week1/pgm1/pgm1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,m;
+    cin >> n>> m;
+    cout<< n-m;
+    return 0;
+}
+
